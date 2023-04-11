8347584bf74678f3da00a9c072984058e6d90ae9
quantumania23 <116637399+Quantumania23@users.noreply.github.com>
2023-04-11 20:08:00 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -18,6 +18,41 @@ arr.forEach(button => {
             string = string.substring(0, string.length-1);
             input.value = string;
         }
+         else if(e.target.innerHTML == 'sin') {
+            var x = input.value;
+            x = x * Math.PI / 180;
+            input.value = Math.sin(x);
+            inputValue = input.value
+         }
+
+         else if(e.target.innerHTML == 'log') {
+            var x = input.value;
+            x = x * Math.PI / 180;
+            input.value = Math.log(x);
+            inputValue = input.value
+        }
+
+        else if(e.target.innerHTML == 'cos') {
+            var x = input.value;
+            x = x * Math.PI / 180;
+            input.value = Math.cos(x);
+            inputValue = input.value
+        }
+
+        else if(e.target.innerHTML == 'tan') {
+            var x = input.value;
+            x = x * Math.PI / 180;
+            input.value = Math.tan(x);
+            inputValue = input.value
+        }
+
+        else if(e.target.innerHTML == 'sqrt') {
+            var x = input.value;
+            x = x * Math.PI / 180;
+            input.value = Math.sqrt(x);
+            inputValue = input.value
+        }
+ 
         else {
             string += e.target.innerHTML;
             input.value =string;
